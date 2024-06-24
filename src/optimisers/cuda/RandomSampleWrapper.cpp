73385066a800fac47501f901c15e46305fe55b94
Matthew Holmes <matthew.holmes111@gmail.com>
2024-06-24 13:11:04 +0100

    Memory allocation and freeing for the random sampling kernel

--- src/optimisers/cuda/RandomSampleWrapper.cpp
+++ src/optimisers/cuda/RandomSampleWrapper.cpp
@@ -12,7 +12,7 @@ std::pair<std::vector<double>, double> GetBestRandomSample(
     std::vector<double>& yDiff,
     double a, /* explore vs exploit */
     const std::vector<double>& lb,
-    const std::vector<double>& ubi,
+    const std::vector<double>& ub,
     const int seed) {
     
 
@@ -26,9 +26,14 @@ std::pair<std::vector<double>, double> GetBestRandomSample(
     
     // these params are tuned for a GeForce 2060
     // TODO - collect info automatically
+    // TODO - update according to the number of samples, since don't
+    // want to exceed total device memory
     int warpSize = 32; // threads should be a multiple of this
     int batchSize = 3000; 
     int threadCount = warpSize * batchSize;
+
+    // each thread is responsible for one random sample surrogate
+    // merit function evaluation
 
     float *V_d, *D_d, *W_d, *muPred_d, *sgPred_d, *innerMerit_d;
     float *S_d, *yDiff_d;
@@ -40,22 +45,93 @@ std::pair<std::vector<double>, double> GetBestRandomSample(
     Vstride = (int)lb.size();
     Dstride = (int)S.size();
 
-    int size = threadCount * Vstride;
+    // ________________________________________________________________________
+    // ****************** DEVICE MEMORY ALLOCATION ****************************
+    // ‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾
 
-    cudaMalloc((void**)&V_d, size * sizeof(float));    
+    // concatentated vectors in search space
+    cudaMalloc((void**)&V_d, Vstride * threadCount * sizeof(float));    
+    // concatenated vectors in distance/weights space
+    cudaMalloc((void**)&D_d, DStride * threadCount * sizeof(float));
+    cudaMalloc((void**)&W_d, DStride * threadCount * sizeof(float));
+    // concatenated predicted values
+    cudaMalloc((void**)&muPred_d,     threadCount * sizeof(float));
+    cudaMalloc((void**)&sgPred_d,     threadCount * sizeof(float));
+    cudaMalloc((void**)&innerMerit_d, threadCount * sizeof(float));
 
-    fillRandomVectors(seed, V_d, size, 10000); // have each rng do 10000 gens        
-    std::vector<float> h_V(size);
+    // copies of values passed to functions, for device access
+    cudaMalloc((void**)&S_d,     Vstride * Dstride * sizeof(float));
+    cudaMalloc((void**)&yDiff_d, Dstride           * sizeof(float));
+    cudaMalloc((void**)&K_d,     Dstride * Dstride * sizeof(float));
+    cudaMalloc((void**)&lb_d,    Vstride           * sizeof(float));
+    cudaMalloc((void**)&ub_d,    Vstride           * sizeof(float));
 
-    cudaMemcpy(h_V.data(), V_d, size * sizeof(float), cudaMemcpyDeviceToHost);
+    // scalars that are used by all threads
+    float h_sg_d = static_cast<float>(sg);
+    cudaMemcpyToSymbol(sg_d, &h_sg_d, sizeof(float));
+    float h_l_d = static_cast<float>(l);
+    cudaMemcpyToSymbol(l_d, &h_l_d, sizeof(float));
+    float h_a_d = static_cast<float>(a);
+    cudaMemcpyToSymbol(a_d, &h_a_d, sizeof(float));
+
+    // ________________________________________________________________________
+    // ********************** TRANSFER INPUT DATA *****************************
+    // ‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾
+
+    std::vector<float> S_flattened;
+    for (const auto& vec: S) {
+        for (int = 0; i < vec.size(); ++i) {
+            S_flattened.push_bac(static_cast<float>(vec[i]));
+        }
+    } 
+    cudaMemcpy(S_d, S_flattened.data(), S_flattened.size() * sizeof(float), 
+        cudaMemcpyHostToDevice);
     
-    std::vector<double> randomDoubles(size);
+    int Kdim = K.rows(); // K is square
+    std::vector<float> K_flattened(Kdim * Kdim);
+    for (int i = 0; i < Kdim; ++i) {
+        for (int j = 0; j < Kdim; ++j) {
+            K_flattened.push_back(static_cast<float>(K(i,j));
+        }
+    }
+    cudaMemcpy(K_d, K_flattened.data(), K_flattened.size() * sizeof(float),
+        cudaMemcpyHostToDevice);
 
-    for (int i = 0; i < size; i++) {
-        randomDoubles[i] = static_cast<double>(h_V[i]);
+    std::vector<float> ub_float, lb_float, yDiff_float;
+
+    for (decltype(lb.size()) i = 0; i != lb.size(); ++i) {
+        lb_float.push_back(static_cast<float>(lb[i]);
+        ub_float.push_back(static_cast<float>(ub[i]);
+    }   
+
+    for (decltype(yDiff.size()) i = 0; i != yDiff.size(); ++i) {
+        yDiff_float.push_back(static_cast<float>(yDiff[i]);
     }
 
+    cudaMemcpy(lb_d, lb_float.data(),       Vstride * sizeof(float),
+        cudaMemcpyHostToDevice);
+    cudaMemcpy(ub_d, ub_float.data(),       Vstride * sizeof(float), 
+        cudaMemcpyHostToDevice);
+    cudaMemcpy(yDiff_d, yDiff_float.data(), Dstride * sizeof(float),
+        cudaMemcpyHostToDevice);
+        
+    // use a separate kernel for the random allocation
+    // since these spin up an RNG state, so don't want one per thread
+    // which is the breakdown for the main kernel
+    
+    fillRandomVectors(seed, V_d, size, 10000); // have each rng do 10000 gens        
+
     cudaFree(V_d);
+    cudaFree(D_d);
+    cudaFree(W_d);
+    cudaFree(muPred_d);
+    cudaFree(sgpred_d);
+    cudaFree(innerMerit_d);
+    cudaFree(S_d);
+    cudaFree(K_d);
+    cudaFree(lb_d);
+    cudaFree(ub_d);
+    cudaFree(yDiff_d);
 
     return std::make_pair(randomDoubles, 0.0);
     
