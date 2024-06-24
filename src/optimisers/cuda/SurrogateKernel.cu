dc13b34c77f9a64997332229ab8ad687b0b05296
Matthew Holmes <matthew.holmes111@gmail.com>
2024-06-24 15:41:50 +0100

    First pass at CUDA kernel for Kriging interpolation inner merit computation

--- src/optimisers/cuda/SurrogateKernel.cu
+++ src/optimisers/cuda/SurrogateKernel.cu
@@ -4,8 +4,7 @@
 
 
 // CUDA kernel
-// TODO - constant arrays??
-__global__ void computeKernel(
+__global__ void innerEvaluationsKernel(
     float* V, int Vstride,
     float* D, int Dstride,
     float* W,
@@ -24,19 +23,49 @@ __global__ void computeKernel(
     
     // map the random values into the bounds 
     // since currently in the range [0,1]
-    int vi = i * Vstride;
+    int vi = i * Vstride; // offset in concatenated sample space vectors
     for (int j = 0; j != Vstride; j++) {
         V[vi + j] = lb[j] + (ub[j] - lb[j]) * V[vi + j]
     } 
 
     // fill Dstride distances using S and kernel params
+    int di = i * Dstride; // offset in concatenated weight space vectors
+    for (int j = 0; j != Dstride; ++j) {
+        // compute Euclidean distance for sample j
+        float dj = 0.0; 
+        for (int k = 0; k != Vstride; ++k) {
+            float djk =  S[j * Vstride + k] - V[vi + k];
+            dj += djk * djk;
+        }        
+        dj = sqrtf(dj); // euclidean distance
+        dj = sg * sg * expf( (-1.0 * dj) / (2.0 * l * l));
+        D[di + j] = dj;
+    }
 
-    // compute Dstride weight values
+    // compute Dstride weight values, matrix vector multiplication
+    for (int j = 0; j != Dstride; ++j) {
+        float wd = 0.0;
+        for (int k = 0; k != DStride; ++k) {
+            wd += K[j*Dstride + k] * D[di + k];
+        }
+        W[di + j] = wd;
+    }
 
     // compute muPred
-
+    float mu_i = 0.0
+    for (int j = 0; j != Dstride; ++j) {
+        mu_i += W[di + j] * W[di + j];    
+    }
+    muPred[i] = mu_i;
+    
     // compute sgPred
-
-    // compute inner merit
+    float dot_i= 0.0;
+    for (int j = 0; j != Dstride; ++j) {
+        dot_i += W[di + j] * D[di + j];
+    }
+    sgPred[i] = sg - sqrtf(dot_i);
+    
+    // compute inner merit, using explore vs exploit coefficient
+    innerMerit[i] = muPred[i] - a * sgPred[i];
 
 }
